1c09a943f437c6dd85151df335970a97154aadd9
zacmiller26 <zacmiller26@icloud.com>
2025-01-16 12:35:08 -0800

    feat: add toasts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+import './globals.css'
+
 import { FULL_NAME } from '@/lib/constants'
 import { cn } from '@/lib/utils'
 import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import { Toaster } from 'sonner'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -32,6 +34,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <Toaster position='top-right' />
       </body>
     </html>
   )

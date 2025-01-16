e40aeae18cf3b0b5c230593a32071a97ce32484e
zacmiller26 <zacmiller26@icloud.com>
2025-01-16 13:01:59 -0800

    feat: email address var + button

--- components/page-links.tsx
+++ components/page-links.tsx
@@ -1,6 +1,11 @@
+'use client'
+
+import useCopyToClipboard from '@/hooks/useCopyToClipboard'
+import { EMAIL_ENCODED, FIRST_NAME } from '@/lib/constants'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import { FaEnvelope, FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import { toast } from 'sonner'
 
 const links = [
   {
@@ -17,13 +22,16 @@ const links = [
     href: 'https://instagram.com/zacmiller26',
     label: 'Instagram',
     icon: FaInstagram
-  },
-  {
-    href: '/',
-    label: 'Email',
-    icon: FaEnvelope
   }
 ]
+
+const buttonClassName = cn(
+  'bg-accent-1 flex items-center gap-2 rounded-full p-2 font-mono text-xs font-semibold text-background',
+  'lg:px-3 lg:py-1',
+  'md:hover:bg-accent'
+)
+
+const iconClassName = 'h-6 w-6 lg:h-3 lg:w-3'
 
 export default function PageLinks() {
   return (
@@ -35,18 +43,32 @@ export default function PageLinks() {
           <Link
             key={link.label}
             href={link.href}
-            className={cn(
-              'bg-accent flex items-center gap-2 rounded-full p-2 font-mono text-xs font-semibold text-background',
-              'lg:px-3 lg:py-1',
-              'md:hover:bg-accent-1'
-            )}
+            className={buttonClassName}
             target='_blank'
           >
-            <Icon className='h-6 w-6 lg:h-3 lg:w-3' />
+            <Icon className={iconClassName} />
             <span className='hidden lg:inline-block'>{link.label}</span>
           </Link>
         )
       })}
+      <EmailAddressButton />
     </div>
+  )
+}
+
+function EmailAddressButton() {
+  const [_, copyToClipboard] = useCopyToClipboard()
+
+  const handleClick = async () => {
+    const email = atob(EMAIL_ENCODED)
+    await copyToClipboard(email)
+    toast.success(`${FIRST_NAME}'s email address was copied!`)
+  }
+
+  return (
+    <button className={buttonClassName} onClick={handleClick}>
+      <FaEnvelope className='h-6 w-6 lg:h-3 lg:w-3' />
+      Email
+    </button>
   )
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const FIRST_NAME = 'Zac'
 export const LAST_NAME = 'Miller'
 export const FULL_NAME = `${FIRST_NAME} ${LAST_NAME}`
+export const EMAIL_ENCODED = 'emFjbWlsbGVyNzJAZ21haWwuY29t'

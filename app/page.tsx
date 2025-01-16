c5f21ca93bb8a2404c1333ad936db65f769dbdba
zacmiller26 <zacmiller26@icloud.com>
2025-01-16 13:48:40 -0800

    fix: improve ui spacing on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { cn } from '@/lib/utils'
 export default function Home() {
   return (
     <div className='boder flex h-[100dvh] flex-col font-[family-name:var(--font-geist-sans)]'>
-      <div className='flex h-full flex-col items-center justify-center gap-8 px-6 pt-12'>
+      <div className='flex h-full flex-col items-center justify-center gap-6 px-2 pt-4 md:gap-8 md:px-6 md:pt-12'>
         <div className='flex flex-col items-center gap-1'>
           <div className='pb-2'>
             <PageAvatar />
@@ -22,13 +22,13 @@ export default function Home() {
 
         <main
           className={cn(
-            'border-background-1 relative w-full min-w-80 flex-1 overflow-scroll rounded-md border p-3 lg:w-[620px] lg:p-8'
+            'relative w-full min-w-80 flex-1 overflow-hidden pb-2 lg:w-[620px]'
           )}
         >
           <Chat />
         </main>
 
-        <footer className='flex gap-6 py-3 sm:pb-8'></footer>
+        <footer className=''>{/* TODO: Drawer Button */}</footer>
       </div>
     </div>
   )

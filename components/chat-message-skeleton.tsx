bdcfe987447cf7d94a89471f6e784ba978388b18
zacmiller26 <zacmiller26@icloud.com>
2025-01-16 13:50:05 -0800

    feat: various chat component improvements
    - better styling
    - added submit form button
    - check for empty message before sending
    - send isLoading prop to most recent message
    - remove chat message skeleton

--- components/chat-message-skeleton.tsx
+++
@@ -1,3 +0,0 @@
-export default function ChatMessageSkeleton() {
-  return <div className='bg-background-2 w-1/4 animate-pulse rounded-md p-2' />
-}
--- components/chat.tsx
+++ components/chat.tsx
@@ -1,13 +1,11 @@
-// /app/components/Chat.tsx
-
 'use client'
 
 import ChatMessage from '@/components/chat-message'
-import ChatMessageSkeleton from '@/components/chat-message-skeleton'
 import { useScrollToBottom } from '@/hooks/useScrollToBottom'
 import { cn } from '@/lib/utils'
 import { useChat } from 'ai/react'
 import { FormEvent } from 'react'
+import { FaCircleArrowUp } from 'react-icons/fa6'
 import { toast } from 'sonner'
 
 export default function Chat() {
@@ -27,37 +25,53 @@ export default function Chat() {
 
   async function onSubmit(e: FormEvent) {
     e.preventDefault()
+
+    if (input.length === 0) return
+
     handleSubmit()
   }
 
   return (
-    <div className='relative flex h-full w-full flex-col'>
-      <div className='flex flex-1 flex-col gap-1.5' ref={messagesContainerRef}>
-        {messages.map(m => (
-          <ChatMessage
-            key={m.id}
-            role={m.role === 'user' ? 'user' : 'ai'}
-            message={m.content}
-          />
-        ))}
+    <div className='relative flex h-full w-full flex-col gap-4 overflow-hidden'>
+      <div
+        className='border-background-2 flex flex-1 flex-col gap-1.5 overflow-scroll rounded-xl border p-3 pb-0 md:p-8'
+        ref={messagesContainerRef}
+      >
+        {messages.map((m, i) => {
+          const isLastMessage = i === messages.length - 1
 
-        {isLoading && <ChatMessageSkeleton />}
+          return (
+            <ChatMessage
+              key={m.id}
+              role={m.role === 'user' ? 'user' : 'ai'}
+              message={m.content}
+              isLoading={isLoading && isLastMessage}
+            />
+          )
+        })}
 
-        <div
-          ref={messagesEndRef}
-          className='min-h-[68px] min-w-[24px] shrink-0'
-        />
+        <div ref={messagesEndRef} className='min-h-4 w-full' />
       </div>
 
-      <form onSubmit={onSubmit} className={cn('sticky bottom-0 flex w-full')}>
+      <form
+        onSubmit={onSubmit}
+        className={cn(
+          'bg-background-1 border-background-2 sticky bottom-0 flex w-full items-center rounded-xl border p-4 shadow transition-colors duration-300',
+          'focus-within:border-foreground-1'
+        )}
+      >
         <input
           className={cn(
-            'block flex-1 rounded border border-foreground bg-background p-2 text-foreground outline-none transition-colors duration-300',
-            'focus:border-accent'
+            'bg-background-1 block flex-1 text-foreground outline-none',
+            'placeholder:text-foreground'
           )}
           value={input}
           placeholder='Ask a question about Zac...'
           onChange={handleInputChange}
+        />
+        <FaCircleArrowUp
+          className={cn('h-6 w-6 cursor-pointer', 'md:hover:text-accent')}
+          onClick={onSubmit}
         />
       </form>
     </div>
